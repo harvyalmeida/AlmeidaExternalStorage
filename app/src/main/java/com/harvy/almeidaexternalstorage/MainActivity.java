b56e3c87f246b837155a81a724bcaf5cadc21e92
harvyalmeida <harvyalmeida@yahoo.com>
2017-11-10 00:03:08 +0800

    Initial commit

--- app/src/main/java/com/harvy/almeidaexternalstorage/MainActivity.java
+++ app/src/main/java/com/harvy/almeidaexternalstorage/MainActivity.java
@@ -1,69 +1,129 @@
 package com.harvy.almeidaexternalstorage;
 
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Environment;
+        import android.preference.PreferenceManager;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+        import java.io.File;
+        import java.io.FileNotFoundException;
+        import java.io.FileOutputStream;
+        import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etMessage;
+    EditText et_Data, et_Filename;
+    Button btn_Shared, btn_IS, btn_IC, btn_EC, btn_ES, btn_EPS, btn_Next;
+    FileOutputStream fos;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etMessage = (EditText) findViewById(R.id.etMessage);
+
+        et_Data = (EditText) findViewById(R.id.etMessage);
+        et_Filename = (EditText) findViewById(R.id.etFilename);
+        btn_Shared = (Button) findViewById(R.id.btnSP);
+        btn_IS = (Button) findViewById(R.id.btnIS);
+        btn_IC = (Button) findViewById(R.id.btnIC);
+        btn_EC = (Button) findViewById(R.id.btnEC);
+        btn_ES = (Button) findViewById(R.id.btnES);
+        btn_EPS = (Button) findViewById(R.id.btnEPS);
+        btn_Next = (Button) findViewById(R.id.btnNext);
     }
 
-    public void saveInternalCache (View view) {
+    public void next (View view) {
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(intent);
+    }
+
+    private void writeData(File file, String message){
+        fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            fos.write(message.getBytes());
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void saveSharedPreference (View view) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("data", et_Data.getText().toString());
+        editor.commit();
+        Toast.makeText(this, "Preferences Saved!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void saveInternalStorage (View view) {
+        String message = et_Data.getText().toString();
+        String filename = et_Filename.getText().toString();
+        try {
+            fos = openFileOutput(filename + ".txt", Context.MODE_PRIVATE);
+            fos.write(message.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(this, "Storage saved!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void saveInternalCache(View view){
+        String filename = et_Filename.getText().toString();
         File folder = getCacheDir();
-        File file = new File(folder, "myText1.txt");
-        FileOutputStream fos = null;
-        String message = etMessage.getText().toString();
-        try {
-            fos = new FileOutputStream(file);
-            fos.write(message.getBytes());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            try {
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        Toast.makeText(this,"Successfully written to internal cache!", Toast.LENGTH_LONG).show();
+        File file = new File(folder, filename + ".txt");
+        String message = et_Data.getText().toString();
+        writeData(file, message);
+        Toast.makeText(this,"Successfully writtin to internal cache!", Toast.LENGTH_LONG).show();
     }
 
-    public void saveExternalCache(View view){
-        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        File file = new File(folder, "myText2.txt");
-        FileOutputStream fos = null;
-        String message = etMessage.getText().toString();
-        try {
-            fos = new FileOutputStream(file);
-            fos.write(message.getBytes());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            try {
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        Toast.makeText(this,"Successfully written to external cache!", Toast.LENGTH_LONG).show();
+    public void saveExternalCache (View view) {
+        String filename = et_Filename.getText().toString();
+        File folder = getExternalCacheDir();
+        File file = new File(folder, filename + ".txt");
+        String message = et_Data.getText().toString();
+        writeData(file, message);
+        Toast.makeText(this, "Successfully written to external cache!", Toast.LENGTH_LONG).show();
+    }
 
+    public void saveExternalStorage (View view) {
+        String filename = et_Filename.getText().toString();
+        File folder = getExternalFilesDir("<NAME HERE>");
+        File file = new File(folder, filename + ".txt");
+        String message = et_Data.getText().toString();
+        writeData(file, message);
+        Toast.makeText(this, "Successfully written to external storage!", Toast.LENGTH_LONG).show();
+    }
+
+    public void saveExternalPublic (View view) {
+        String filename = et_Filename.getText().toString();
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File (folder, filename + ".txt");
+        String message = et_Data.getText().toString();
+        writeData(file, message);
+        Toast.makeText(this, "Successfully written to external public storage!", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/harvy/almeidaexternalstorage/SecondActivity.java
+++ app/src/main/java/com/harvy/almeidaexternalstorage/SecondActivity.java
@@ -1,8 +1,156 @@
 package com.harvy.almeidaexternalstorage;
 
-/**
- * Created by harvyalmeida on 09/11/2017.
- */
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Environment;
+        import android.preference.PreferenceManager;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-public class SecondActivity {
+        import java.io.BufferedReader;
+        import java.io.File;
+        import java.io.FileInputStream;
+        import java.io.FileNotFoundException;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView tv_Data;
+    FileInputStream fis;
+    BufferedReader br;
+    Button btn_Shared;
+    Button btn_IS;
+    View btn_IC;
+    Button btn_EC;
+    Button btn_ES;
+    Button btn_EPS;
+    Button btn_Next;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        tv_Data = (TextView) findViewById(R.id.tvData);
+        btn_Shared = (Button) findViewById(R.id.btnSP);
+        btn_IS = (Button) findViewById(R.id.btnIS);
+        btn_IC = findViewById(R.id.btnIC);
+        btn_EC = (Button) findViewById(R.id.btnEC);
+        btn_ES = (Button) findViewById(R.id.btnES);
+        btn_EPS = (Button) findViewById(R.id.btnEPS);
+        btn_Next = (Button) findViewById(R.id.btnNext);
+
+    }
+
+    public void previous (View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadSharedPreference(View view) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());;
+        String data = preferences.getString("data","");
+        tv_Data.setText(data);
+    }
+
+    public void loadInternalStorage (View view) throws IOException {
+        String newline = "";
+        String data = "";
+        try{
+            fis = openFileInput("storage.txt");
+            br = new BufferedReader(new InputStreamReader(fis));
+            if ((newline = br.readLine()) != null)
+                data = newline;
+            fis.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        tv_Data.setText(data);
+    }
+
+    public void loadInternalCache(View view) throws FileNotFoundException {
+        String newline = "";
+        String data = "";
+        File folder = getCacheDir();
+        File file = new File(folder, "storage.txt");
+        fis = new FileInputStream(file);
+        try{
+            br = new BufferedReader(new InputStreamReader(fis));
+            if ((newline = br.readLine()) != null)
+                data = newline;
+            fis.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        tv_Data.setText(data);
+    }
+
+    public void loadExternalCache(View view) throws FileNotFoundException {
+        String newline = "";
+        String data = "";
+        File folder = getExternalCacheDir();
+        File file = new File(folder, "storage.txt");
+        fis = new FileInputStream(file);
+        try{
+            br = new BufferedReader(new InputStreamReader(fis));
+            if ((newline = br.readLine()) != null)
+                data = newline;
+            fis.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        tv_Data.setText(data);
+    }
+
+    public void loadExternalStorage (View view) throws FileNotFoundException {
+        String newline = "";
+        String data = "";
+        File folder = getExternalFilesDir("<PANGALAN MO DITO>");
+        File file = new File(folder, "storage.txt");
+        fis = new FileInputStream(file);
+        try{
+            br = new BufferedReader(new InputStreamReader(fis));
+            if ((newline = br.readLine()) != null)
+                data = newline;
+            fis.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        tv_Data.setText(data);
+    }
+
+    public void loadExternalPublic (View view) throws FileNotFoundException {
+        String newline = "";
+        String data = "";
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File (folder, "storage.txt");
+        fis = new FileInputStream(file);
+        try{
+            br = new BufferedReader(new InputStreamReader(fis));
+            if ((newline = br.readLine()) != null)
+                data = newline;
+            fis.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        tv_Data.setText(data);
+
+    }
 }
